6f493524be5ddafb24a508c2673ece1765526b81
Abbosjon0 <abbosjonanvarov001@gmail.com>
2024-11-18 15:59:58 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# x = np.array([0, 8])
+# y = np.array([0, 350])
+# plt.plot(x, y)
+# plt.show()
+
+# y = np.array([200,240,0,350,0,300,0,270,0,290,0,360,200])
+# plt.plot(y , marker='*')
+# plt.show()
+
+# x = np.array([0, 1, 2, 3, 4, 5])
+# y = np.array([0, 1, 2, 3, 4, 5])
+# mycolor = ['red', 'green', 'purple', 'lime', 'aqua', 'yellow']
+# size = [100, 200, 300, 400, 500, 600]
+# plt.scatter(x, y, color=mycolor, s=size)
+# plt.show()
+
+# x = np.array(['A', 'B', 'C', 'D'])  
+# y = np.array([3, 8, 1, 10])
+# plt.bar(x, y)
+# plt.show()
+
+# x = np.array(['A', 'B', 'C', 'D'])  
+# y = np.array([3, 8, 1, 10])
+# plt.barh(x, y)
+# plt.show()
+
+# x = np.array([35, 25, 25, 15, 20, 10, 40, 30, 5, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 151])
+# plt.pie(x)
+# plt.show()
+
+# x = np.array([35, 25, 25, 15])
+# mylabels = ["Qalam", "Ruchka", "Daftar", "Chizg'ich"]
+# plt.pie(x, labels=mylabels)
+# plt.show()
